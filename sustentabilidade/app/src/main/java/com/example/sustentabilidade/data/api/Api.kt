6c909dfbe7fb0dc6f65dfaf2d0c51ae0f17c7f8a
jadson540 <jhps@discente.ifpe.edu.br>
2025-12-18 20:55:01 -0300

    Delete sustentabilidade directory

--- sustentabilidade/app/src/main/java/com/example/sustentabilidade/data/api/Api.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sustentabilidade.data.api
-
-import com.example.sustentabilidade.data.model.TipsResponse
-import retrofit2.http.GET
-
-interface Api {
-    @GET("tips")
-    suspend fun getTips(): TipsResponse
-}
--- sustentabilidade/app/src/main/java/com/example/sustentabilidade/ui/screens/DetailScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sustentabilidade.ui.screens
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun DetailScreen(id:String){
-    Text("Detalhe da dica: $id")
-}
